8b98434b1ec706291141d6631c2d1733a02116fe
Zefirox <rasbel2000@hotmail.com>
2022-02-14 14:09:58 -0500

    Update de la Clase Model

--- src/co/edu/unbosque/model/Model.java
+++ src/co/edu/unbosque/model/Model.java
@@ -1,4 +1,29 @@
 package co.edu.unbosque.model;
 
+import co.edu.unbosque.model.persistence.FileEdit;
+
+import java.util.ArrayList;
+
 public class Model {
+    private ArrayList<String[]> array = new ArrayList<>();
+    private FileEdit file = new FileEdit();
+
+    public Model(){
+    }
+
+    public ArrayList<String[]> getArray() {
+        return array;
+    }
+
+    public void setArray(ArrayList<String[]> array) {
+        this.array = array;
+    }
+
+    public FileEdit getFile() {
+        return file;
+    }
+
+    public void setFile(FileEdit file) {
+        this.file = file;
+    }
 }
