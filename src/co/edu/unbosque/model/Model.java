2e8159288a51c90c7d09aa6d14b3b8ebe89a5391
Zefirox <rasbel2000@hotmail.com>
2022-02-16 18:20:53 -0500

    Update de la Clase Model

--- src/co/edu/unbosque/model/Model.java
+++ src/co/edu/unbosque/model/Model.java
@@ -1,14 +1,41 @@
 package co.edu.unbosque.model;
 
-import co.edu.unbosque.model.persistence.FileEdit;
-
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class Model {
     private ArrayList<String[]> array = new ArrayList<>();
-    private FileEdit file = new FileEdit();
+    private final File f = new File("src/data.csv");
 
     public Model(){
+    }
+
+    public void setCSV() {
+
+        String line;
+
+        try {
+
+            FileReader fr = new FileReader(f);
+            BufferedReader br = new BufferedReader(fr);
+
+            line = br.readLine();
+
+            while (line!=null){
+                String[] aux = line.split(",");
+                array.add(aux);
+                line = br.readLine();
+            }
+
+            fr.close();
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public ArrayList<String[]> getArray() {
@@ -17,13 +44,5 @@ public class Model {
 
     public void setArray(ArrayList<String[]> array) {
         this.array = array;
-    }
-
-    public FileEdit getFile() {
-        return file;
-    }
-
-    public void setFile(FileEdit file) {
-        this.file = file;
     }
 }
