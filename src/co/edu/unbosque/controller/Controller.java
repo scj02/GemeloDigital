0fb822cd593151c77b1e4fdf3d8af1441592acf3
sago1 <ccruzj@unbosque.edu.co>
2022-02-23 18:18:23 -0500

    Controller casi terminado

---
+++ src/co/edu/unbosque/controller/Controller.java
@@ -0,0 +1,53 @@
+package co.edu.unbosque.controller;
+
+import co.edu.unbosque.model.Model;
+import co.edu.unbosque.model.Register;
+import co.edu.unbosque.view.View;
+
+public class Controller {
+
+    private View v;
+    private Model m;
+
+
+    public Controller() {
+        v = new View();
+        m = new Model();
+        funcionar();
+    }
+
+    public void funcionar() {
+        m.uploadData();
+
+        for (Register register : m.getArray()) {
+            System.out.println(register);
+        }
+
+        System.out.println(m.findByInvoiceNo("533677"));
+
+        System.out.println(m.sumTotalSales());
+
+        String searchedNumbers;
+        do {
+            String b = v.pedirString("Ingrese el archivo que desea buscar: ");
+            searchedNumbers = m.findByInvoiceNo(b);
+        }
+        while (searchedNumbers.length() == 0);
+        System.out.println(searchedNumbers);
+
+        System.out.println(m.avgMonthlySales(true));
+
+        System.out.println(m.countByStockCode("22623"));
+
+        String stockNumbers ;
+        do {
+            String b = v.pedirEntero();
+            stockNumbers = m.findByInvoiceNo(b);
+        }
+        while (stockNumbers.length() == 0);
+        System.out.println(stockNumbers);
+
+    }
+}
+
+
--- src/co/edu/unbosque/model/Model.java
+++ src/co/edu/unbosque/model/Model.java
@@ -1,14 +1,15 @@
 package co.edu.unbosque.model;
 
-import java.io.*;
+
+import com.opencsv.CSVReader;
+
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 
-import com.opencsv.*;
-import com.opencsv.exceptions.CsvValidationException;
-
-public class Model{
+public class Model {
     private ArrayList<Register> array = new ArrayList<>();
     private final File f = new File("src/data.csv");
 
@@ -27,146 +28,136 @@ public class Model{
 
             line = reader.readNext();
 
-            while (line!=null){
+            while (line != null) {
 
-                array.add(new Register(line[0], line[1], line[2], line[3], line[4],line[5], line[6], line[7] ));
+                array.add(new Register(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]));
                 line = reader.readNext();
             }
             array.remove(0);
             reader.close();
-        }
-        catch (CsvValidationException ex) {
-
-        }
-        catch (FileNotFoundException ex) {
-
-        }
-        catch (IOException ex) {
-
+        } catch (Exception ex) {
+            ex.printStackTrace();
         }
 
     }
 
-    public float sumTotalSales(){
+    public float sumTotalSales() {
         float total = 0;
 
-        for(int i=0;i<array.size();i++){
+        for (int i = 0; i < array.size(); i++) {
             Register aux = array.get(i);
             float x = Float.parseFloat(aux.getQty());
             float y = Float.parseFloat(aux.getUnitPrice());
 
-            float mul = x*y;
+            float mul = x * y;
 
-            total+=mul;
+            total += mul;
         }
 
         return total;
     }
 
-    public String findByInvoiceNo(String invoiceNo){
-        String items="";
-        for(int i=0;i<array.size();i++){
+    public String findByInvoiceNo(String invoiceNo) {
+        String items = "";
+        for (int i = 0; i < array.size(); i++) {
             Register aux = array.get(i);
-            if (aux.getInvoiceNo().equals(invoiceNo)){
+            if (aux.getInvoiceNo().equals(invoiceNo)) {
 
-                items += aux +"\n";
+                items += aux + "\n";
             }
         }
 
         return items;
     }
 
-    public int countByStockCode(String stockCode){
+    public int countByStockCode(String stockCode) {
 
-        int counter=0;
+        int counter = 0;
 
-        for(int i=0;i<array.size();i++){
+        for (int i = 0; i < array.size(); i++) {
             Register aux = array.get(i);
-            if (aux.getStockCode().equals(stockCode)){
-                counter+=Integer.parseInt(aux.getQty());
+            if (aux.getStockCode().equalsIgnoreCase(stockCode)) {
+                counter += Integer.parseInt(aux.getQty());
             }
         }
 
         return counter;
     }
 
-    public String avgMonthlySales(boolean groupByCountry){
+    public String avgMonthlySales(boolean groupByCountry) {
 
         ArrayList<String> country = new ArrayList();
         country.add("");
         ArrayList<Float> totalByCountry = new ArrayList<Float>();
         totalByCountry.add((float) 0);
-        int j=1;
-        int in=0;
+        int j = 1;
+        int in = 0;
 
-       if(groupByCountry==false){
+        if (!groupByCountry) {
+            float total = sumTotalSales();
+            float avg = total / 12;
+            return "Promedio mensual: " + avg;
 
-           float total = sumTotalSales();
-           float avg = total/12;
-           String out = "Promedio mensual: "+String.valueOf(avg);
-           return out;
+        } else {
+            for (int i = 0; i < array.size(); i++) {
+                Register aux = array.get(i);
+                int index = country.indexOf(aux.getCountry());
+                if (index == -1) {
+                    country.add(index + j, aux.getCountry());
+                    j++;
+                }
+            }
+            for (int i = 0; i < array.size(); i++) {
+                Register aux = array.get(i);
+                int index = country.indexOf(aux.getCountry());
 
-       }else if(groupByCountry){
-           for(int i=0;i<array.size();i++){
-               Register aux = array.get(i);
-               int index = country.indexOf(aux.getCountry());
-               if(index==-1){
-                  country.add(index+j, aux.getCountry());
-                  j++;
-               }
-           }
-           for(int i=0;i<array.size();i++){
-               Register aux = array.get(i);
-               int index = country.indexOf(aux.getCountry());
+                float x = Float.parseFloat(aux.getQty());
+                float y = Float.parseFloat(aux.getUnitPrice());
 
-               float x = Float.parseFloat(aux.getQty());
-               float y = Float.parseFloat(aux.getUnitPrice());
+                float mul = x * y;
+                if (index <= in) {
+                    float total = totalByCountry.get(index) + mul;
+                    totalByCountry.remove(index);
+                    totalByCountry.add(index, total);
+                } else {
+                    in++;
+                    float total = mul;
+                    totalByCountry.add(index, total);
+                }
 
-               float mul = x*y;
-               if(index<=in){
-                   float total = totalByCountry.get(index)+mul;
-                   totalByCountry.remove(index);
-                   totalByCountry.add(index, total);
-               }else{
-                   in++;
-                   float total =mul;
-                   totalByCountry.add(index, total);
-               }
+            }
 
-           }
+            float total = sumTotalSales();
+            float average = total / 12;
+            String out = "Average Monthly Sales: " + String.valueOf(average) + "\n";
 
-           float total = sumTotalSales();
-           float average = total/12;
-           String out = "Average Monthly Sales: "+String.valueOf(average)+"\n";
+            for (int i = 0; i < totalByCountry.size(); i++) {
+                float avg = totalByCountry.get(i) / 12;
+                totalByCountry.remove(i);
+                totalByCountry.add(i, avg);
 
-           for(int i=0;i<totalByCountry.size();i++){
-               float avg = totalByCountry.get(i)/12;
-               totalByCountry.remove(i);
-               totalByCountry.add(i, avg);
-
-               out += country.get(i)+": "+totalByCountry.get(i)+"\n";
-           }
+                out += country.get(i) + ": " + totalByCountry.get(i) + "\n";
+            }
 
 
-           return out;
-       }
+            return out;
+        }
 
-       return "";
     }
 
-    public String findPartiallyByDescription(String search, boolean order, int intMonth, int endMonth){
+    public String findPartiallyByDescription(String search, boolean order, int intMonth, int endMonth) {
 
-        if (order==false){
-            for(int i=0;i<array.size();i++){
+        if (order == false) {
+            for (int i = 0; i < array.size(); i++) {
                 Register aux = array.get(i);
-                String invoiceDate = aux.getDate().split("/",2)[0];
+                String invoiceDate = aux.getDate().split("/", 2)[0];
                 if (aux.getDescription().contains(search)) {
-                    if(Integer.parseInt(invoiceDate)>=intMonth&&Integer.parseInt(invoiceDate)<=endMonth){
-                        System.out.println(aux.getDescription()+": "+aux.getQty());
+                    if (Integer.parseInt(invoiceDate) >= intMonth && Integer.parseInt(invoiceDate) <= endMonth) {
+                        System.out.println(aux.getDescription() + ": " + aux.getQty());
                     }
                 }
             }
-        }else if (order){
+        } else if (order) {
             Collections.sort(array, new Comparator<Register>() {
                 @Override
                 public int compare(Register o1, Register o2) {
@@ -174,17 +165,16 @@ public class Model{
                 }
             });
 
-            for(int i=0;i<array.size();i++){
+            for (int i = 0; i < array.size(); i++) {
                 Register aux = array.get(i);
-                String invoiceDate = aux.getDate().split("/",2)[0];
+                String invoiceDate = aux.getDate().split("/", 2)[0];
                 if (aux.getDescription().contains(search)) {
-                    if(Integer.parseInt(invoiceDate)>=intMonth&&Integer.parseInt(invoiceDate)<=endMonth){
-                        System.out.println(aux.getDescription()+": "+aux.getQty());
+                    if (Integer.parseInt(invoiceDate) >= intMonth && Integer.parseInt(invoiceDate) <= endMonth) {
+                        System.out.println(aux.getDescription() + ": " + aux.getQty());
                     }
                 }
             }
         }
-
 
 
         return "";
